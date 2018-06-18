1883b9968e8aca568c27913ddfe51b5b77224054
Isabel Hirama <isabel.hirama@gmail.com>
2018-06-18 16:58:38 -0400

    Created database and tables

--- models.js
+++ models.js
@@ -3,3 +3,49 @@ const Sequelize = require("sequelize");
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
 module.exports = db;
+
+const Gardener = db.define('gardener', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+const Plot = db.define('plot', {
+  size: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+})
+
+const Vegetable = db.define('vegetable', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  planted_on: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
+})
+
+Plot.belongsTo(Gardener, {as: 'gardener'})
+
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'})
+
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
+
+
